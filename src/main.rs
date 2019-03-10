0abdbe0ec94b4168e6932a64951883eef3454a3d
Christopher Miller <admin@mctherealm.net>
2019-03-10 15:24:59 -0400

    Created boilerplate program for messing with command arguments and colors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use ansi_term::Colour::{Red, Yellow};
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("{}", Red.paint("Expected arguments COLOR and <MESSAGE> passed!"));
+        return
+    }
+    println!("{}", Yellow.paint(format!("The color you passed: {}", args[1])));
 }
