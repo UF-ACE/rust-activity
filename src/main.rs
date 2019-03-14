c48d7598a7e8e2fe7a068e445ee62328a20867ad
Christopher Miller <admin@mctherealm.net>
2019-03-14 12:51:53 -0400

    Changed wording of error

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!("{}", Red.paint("Expected arguments COLOR and <MESSAGE> passed!"));
+        println!("{}", Red.paint("Expected arguments COLOR and <MESSAGE>!"));
         return
     }
     println!("{}", Yellow.paint(format!("The color you passed: {}", args[1])));
